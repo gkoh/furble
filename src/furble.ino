87f297f8fd5fbe6294699c3bf734ef8debf96555
Guo-Rong Koh <gkoh@users.noreply.github.com>
2023-03-19 14:50:46 +1030

    Hook up the M5 GPS unit via the Grove UART.
    Add a settings menu entry to show real-time GPS information.
    GPS is active as soon as system is started.
    Must be in 'Remote Control' shutter menu for GPS data to be properly
    updated.
    
    This is still a work in progress, but looks like the major pieces are
    working.
    Battery life is atrocious at this stage.
    
    LED will flash slowly to indicate GPS fix is pending.
    Flash speed will increase when GPS fix is acquired.
    If GPS updates are not received after 60 seconds, go back to pending.

--- src/furble.ino
+++ src/furble.ino
@@ -1,6 +1,7 @@
 #include <Furble.h>
 #include <M5ez.h>
 #include <NimBLEDevice.h>
+#include <TinyGPS++.h>
 
 #ifdef M5STICKC_PLUS
 #include <M5StickCPlus.h>
@@ -14,12 +15,73 @@ static NimBLEScan *pScan = nullptr;
 
 static std::vector<Furble::Device *> connect_list;
 
+static TinyGPSPlus gps;
+HardwareSerial GroveSerial(2);
+static const uint32_t GPS_BAUD = 9600;
+static const uint16_t GPS_SERVICE_MS = 250;
+static const uint16_t GPS_NO_FIX_LED_MS = 500;
+static const uint16_t GPS_FIX_LED_MS = 200;
+static const uint32_t GPS_MAX_AGE = 60 * 1000;
+
+static bool gps_has_fix = false;
+
+/**
+ * BLE Advertisement callback.
+ */
 class AdvertisedCallback: public NimBLEAdvertisedDeviceCallbacks {
   void onResult(NimBLEAdvertisedDevice *pDevice) {
     Furble::Device::match(pDevice, connect_list);
     ez.msgBox("Scanning", "Found ... " + String(connect_list.size()), "", false);
   }
 };
+
+/**
+ * GPS serial event service handler.
+ */
+static uint16_t service_grove_gps(void) {
+  while (Serial2.available() > 0) {
+    gps.encode(Serial2.read());
+  }
+
+  if ((gps.location.age() < GPS_MAX_AGE) && gps.location.isValid() &&
+    (gps.date.age() < GPS_MAX_AGE) && gps.date.isValid() &&
+    (gps.time.age() < GPS_MAX_AGE) && gps.time.age()) {
+    gps_has_fix = true;
+  }
+  else {
+    gps_has_fix = false;
+  }
+
+  return GPS_SERVICE_MS;
+}
+
+/**
+ * GPS fix LED flash.
+ */
+static uint16_t service_gps_fix_led(void) {
+  static bool flash = false;
+
+  flash = !flash;
+  digitalWrite(M5_LED, flash ? HIGH : LOW);
+
+  return (gps_has_fix ? GPS_FIX_LED_MS : GPS_NO_FIX_LED_MS);
+}
+
+/**
+ * Update geotag data.
+ */
+static void update_geodata(Furble::Device *device) {
+  if (gps.location.isUpdated() && gps.location.isValid() && gps.date.isUpdated()
+    && gps.date.isValid() && gps.time.isValid() && gps.time.isValid()) {
+    Furble::Device::gps_t dgps = { gps.location.lat(),
+      gps.location.lng(),
+      gps.altitude.meters() };
+    Furble::Device::timesync_t timesync = { gps.date.year(), gps.date.month(),
+    gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second() };
+
+    device->updateGeoData(dgps, timesync);
+  }
+}
 
 /**
  * Display the version.
@@ -39,9 +101,7 @@ static void remote_control(Furble::Device *device) {
   while (true) {
     m5.update();
 
-    Furble::Device::gps_t gps = { -34.935452, -56.160619, -567 };
-    Furble::Device::timesync_t timesync = { 2024, 25, 12, 12, 34, 56 };
-    device->updateGeoData(gps, timesync);
+    update_geodata(device);
 
     // Source code in AXP192 says 0x02 is short press.
     if (m5.Axp.GetBtnPress() == 0x02) {
@@ -64,6 +124,7 @@ static void remote_control(Furble::Device *device) {
       device->focusRelease();
     }
 
+    ez.yield();
     delay(50);
   }
 }
@@ -122,9 +183,7 @@ static void menu_connect(bool save) {
 
   Furble::Device *device = connect_list[i - 1];
 
-  Furble::Device::gps_t gps = { -34.935452, -56.160619, -567 };
-  Furble::Device::timesync_t timesync = { 2024, 25, 12, 12, 34, 56 };
-  device->updateGeoData(gps, timesync);
+  update_geodata(device);
 
   NimBLEClient *pClient = NimBLEDevice::createClient();
   ezProgressBar progress_bar(FURBLE_STR, "Connecting ...", "");
@@ -154,11 +213,41 @@ static void menu_delete() {
   devices[i - 1]->remove();
 }
 
+static void gps_info(void) {
+  Serial.println("GPS Information");
+  char buffer[256] = { 0x0 };
+
+  while (true) {
+    snprintf(buffer, 256, "%s (%d) \n%.2f, %.2f\n%.2f metres\n%4u-%02u-%02u %02u:%02u:%02u\n",
+    gps.location.isValid() && gps.date.isValid() && gps.time.isValid() ? "Valid" : "Invalid",
+    gps.location.age(),
+    gps.location.lat(),
+    gps.location.lng(),
+    gps.altitude.meters(),
+    gps.date.year(),
+    gps.date.month(),
+    gps.date.day(),
+    gps.time.hour(),
+    gps.time.minute(),
+    gps.time.second());
+    ez.msgBox("GPS Information", buffer, "", false);
+    m5.update();
+
+    if (m5.BtnB.wasPressed()) {
+      break;
+    }
+
+    ez.yield();
+    delay(250);
+  }
+}
+
 static void menu_settings(void) {
   ezMenu submenu(FURBLE_STR " - Settings");
 
   submenu.buttons("OK#down");
   submenu.addItem("Backlight", ez.backlight.menu);
+  submenu.addItem("GPS Information", gps_info);
   submenu.addItem("Theme", ez.theme->menu);
   submenu.addItem("Transmit Power", settings_menu_tx_power);
   submenu.addItem("About", about);
@@ -173,12 +262,18 @@ static void mainmenu_poweroff(void) {
 
 void setup() {
   Serial.begin(115200);
+  Serial2.begin(GPS_BAUD, SERIAL_8N1, 33, 32);
+
+  pinMode(M5_LED, OUTPUT);
+  digitalWrite(M5_LED, HIGH);
 
 #include <themes/dark.h>
 #include <themes/default.h>
 #include <themes/mono_furble.h>
 
   ez.begin();
+  ez.addEvent(service_grove_gps, millis() + 500);
+  ez.addEvent(service_gps_fix_led);
   NimBLEDevice::init(FURBLE_STR);
   NimBLEDevice::setSecurityAuth(true, true, true);
 
