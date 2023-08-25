482d765e6ed083c15928f0f934f0dba3583cab26
Guo-Rong Koh <gkoh@users.noreply.github.com>
2023-08-25 17:16:26 +0930

    Add GPS enable setting. Add header widget for GPS fix/nofix.

--- src/furble.ino
+++ src/furble.ino
@@ -15,14 +15,17 @@ static NimBLEScan *pScan = nullptr;
 
 static std::vector<Furble::Device *> connect_list;
 
+bool load_gps_enable();
+
 static TinyGPSPlus gps;
 HardwareSerial GroveSerial(2);
 static const uint32_t GPS_BAUD = 9600;
 static const uint16_t GPS_SERVICE_MS = 250;
-static const uint16_t GPS_NO_FIX_LED_MS = 500;
-static const uint16_t GPS_FIX_LED_MS = 200;
-static const uint32_t GPS_MAX_AGE = 60 * 1000;
+static const uint32_t GPS_MAX_AGE_MS = 60 * 1000;
 
+static const uint8_t  GPS_HEADER_POSITION = LEFTMOST + 1;
+
+static bool gps_enable = false;
 static bool gps_has_fix = false;
 
 /**
@@ -39,13 +42,17 @@ class AdvertisedCallback: public NimBLEAdvertisedDeviceCallbacks {
  * GPS serial event service handler.
  */
 static uint16_t service_grove_gps(void) {
+  if (!gps_enable) {
+    return GPS_SERVICE_MS;
+  }
+
   while (Serial2.available() > 0) {
     gps.encode(Serial2.read());
   }
 
-  if ((gps.location.age() < GPS_MAX_AGE) && gps.location.isValid() &&
-    (gps.date.age() < GPS_MAX_AGE) && gps.date.isValid() &&
-    (gps.time.age() < GPS_MAX_AGE) && gps.time.age()) {
+  if ((gps.location.age() < GPS_MAX_AGE_MS) && gps.location.isValid() &&
+    (gps.date.age() < GPS_MAX_AGE_MS) && gps.date.isValid() &&
+    (gps.time.age() < GPS_MAX_AGE_MS) && gps.time.age()) {
     gps_has_fix = true;
   }
   else {
@@ -56,21 +63,13 @@ static uint16_t service_grove_gps(void) {
 }
 
 /**
- * GPS fix LED flash.
- */
-static uint16_t service_gps_fix_led(void) {
-  static bool flash = false;
-
-  flash = !flash;
-  digitalWrite(M5_LED, flash ? HIGH : LOW);
-
-  return (gps_has_fix ? GPS_FIX_LED_MS : GPS_NO_FIX_LED_MS);
-}
-
-/**
  * Update geotag data.
  */
 static void update_geodata(Furble::Device *device) {
+  if (!gps_enable) {
+    return;
+  }
+
   if (gps.location.isUpdated() && gps.location.isValid() && gps.date.isUpdated()
     && gps.date.isValid() && gps.time.isValid() && gps.time.isValid()) {
     Furble::Device::gps_t dgps = { gps.location.lat(),
@@ -80,6 +79,28 @@ static void update_geodata(Furble::Device *device) {
     gps.date.day(), gps.time.hour(), gps.time.minute(), gps.time.second() };
 
     device->updateGeoData(dgps, timesync);
+  }
+}
+
+/**
+ * Draw GPS enable/fix widget.
+ */
+static void gps_draw_widget(uint16_t x, uint16_t y) {
+  if (!gps_enable) {
+    return;
+  }
+
+  int16_t r = ((ez.theme->header_height - (2 * ez.theme->header_hmargin)) / 2);
+  int16_t cx = x + r;
+  int16_t cy = (ez.theme->header_height / 2);
+
+  if (gps_has_fix) {
+    // With fix, draw solid circle
+    m5.lcd.fillCircle(cx, cy, r, ez.theme->header_fgcolor);
+  }
+  else {
+    // No fix, empty circle
+    m5.lcd.drawCircle(cx, cy, r, ez.theme->header_fgcolor);
   }
 }
 
@@ -213,41 +234,13 @@ static void menu_delete() {
   devices[i - 1]->remove();
 }
 
-static void gps_info(void) {
-  Serial.println("GPS Information");
-  char buffer[256] = { 0x0 };
-
-  while (true) {
-    snprintf(buffer, 256, "%s (%d) \n%.2f, %.2f\n%.2f metres\n%4u-%02u-%02u %02u:%02u:%02u\n",
-    gps.location.isValid() && gps.date.isValid() && gps.time.isValid() ? "Valid" : "Invalid",
-    gps.location.age(),
-    gps.location.lat(),
-    gps.location.lng(),
-    gps.altitude.meters(),
-    gps.date.year(),
-    gps.date.month(),
-    gps.date.day(),
-    gps.time.hour(),
-    gps.time.minute(),
-    gps.time.second());
-    ez.msgBox("GPS Information", buffer, "", false);
-    m5.update();
-
-    if (m5.BtnB.wasPressed()) {
-      break;
-    }
-
-    ez.yield();
-    delay(250);
-  }
-}
 
 static void menu_settings(void) {
   ezMenu submenu(FURBLE_STR " - Settings");
 
   submenu.buttons("OK#down");
   submenu.addItem("Backlight", ez.backlight.menu);
-  submenu.addItem("GPS Information", gps_info);
+  submenu.addItem("GPS", settings_menu_gps);
   submenu.addItem("Theme", ez.theme->menu);
   submenu.addItem("Transmit Power", settings_menu_tx_power);
   submenu.addItem("About", about);
@@ -261,19 +254,19 @@ static void mainmenu_poweroff(void) {
 }
 
 void setup() {
+  gps_enable = load_gps_enable();
+
   Serial.begin(115200);
   Serial2.begin(GPS_BAUD, SERIAL_8N1, 33, 32);
-
-  pinMode(M5_LED, OUTPUT);
-  digitalWrite(M5_LED, HIGH);
 
 #include <themes/dark.h>
 #include <themes/default.h>
 #include <themes/mono_furble.h>
 
   ez.begin();
+  ez.header.insert(GPS_HEADER_POSITION, "gps", ez.theme->header_height -
+  (2 * ez.theme->header_hmargin), gps_draw_widget);
   ez.addEvent(service_grove_gps, millis() + 500);
-  ez.addEvent(service_gps_fix_led);
   NimBLEDevice::init(FURBLE_STR);
   NimBLEDevice::setSecurityAuth(true, true, true);
 
