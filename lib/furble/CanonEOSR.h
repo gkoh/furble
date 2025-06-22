63bc5e950d4888ad382c264d72ee485c0d7b198c
Guo-Rong <5484552+gkoh@users.noreply.github.com>
2025-06-22 11:18:35 +0930

    Implement GPS for Canon EOS R.
    In collaboration with @Jerroder, implement the GPS sync protocol for
    Canon EOS R.

--- lib/furble/CanonEOSR.h
+++ lib/furble/CanonEOSR.h
@@ -24,6 +24,18 @@ class CanonEOSR: public CanonEOS {
   void updateGeoData(const gps_t &gps, const timesync_t &timesync) override final;
 
  private:
+  // Location and time message
+  typedef struct __attribute__((packed)) _canon_geo_t {
+    uint8_t header;               // 0x04
+    uint8_t latitude_direction;   // latitude direction "{N|S}"
+    float latitude;               // degrees latitude (float32)
+    uint8_t longitude_direction;  // longitude direction "{E|W}"
+    float longitude;              // degrees longitude (float32)
+    uint8_t elevation_sign;       // elevation sign "{-|+}"
+    float elevation;              // metres elevation (float32)
+    uint32_t timestamp;           // seconds since epoch
+  } canon_geo_t;
+
   // Primary service
   static const NimBLEUUID PRI_SVC_UUID;
 
@@ -33,11 +45,26 @@ class CanonEOSR: public CanonEOS {
   // Control characteristic (focus, shutter)
   const NimBLEUUID CTRL_CHR_UUID {0x00050003, 0x0000, 0x1000, 0x0000d8492fffa821};
 
+  // Location service
+  const NimBLEUUID GEO_SVC_UUID {0x00040000, 0x0000, 0x1000, 0x0000d8492fffa821};
+
+  // Location characteristic
+  const NimBLEUUID GEO_CHR_UUID {0x00040002, 0x0000, 0x1000, 0x0000d8492fffa821};
+
+  // Location indication
+  const NimBLEUUID GEO_IND_UUID {0x00040003, 0x0000, 0x1000, 0x0000d8492fffa821};
+
   static constexpr uint8_t SHUTTER = 0x80;
   static constexpr uint8_t FOCUS = 0x40;
   static constexpr uint8_t CTRL = 0x0c;
 
-  NimBLERemoteCharacteristic *pControl = nullptr;
+  static constexpr uint8_t GEO_REQUEST = 0x03;
+  const std::array<uint8_t, 1> GEO_ENABLE = {0x01};
+  static constexpr uint8_t GEO_SUCCESS = 0x02;
+
+  NimBLERemoteCharacteristic *m_Control = nullptr;
+  NimBLERemoteCharacteristic *m_Geo = nullptr;
+  bool m_GeoEnabled = false;
 
   bool _connect(void) override final;
 };
