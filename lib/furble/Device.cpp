07d56e040bcfe09c03e425539c52f77398ec248a
Guo-Rong <5484552+gkoh@users.noreply.github.com>
2025-08-06 20:55:17 +0930

    Add numeric display security capability.
    Use the default to auto-accept the displayed PIN.
    This may need to be revisited if users want the actual security.

--- lib/furble/Device.cpp
+++ lib/furble/Device.cpp
@@ -39,6 +39,7 @@ void Device::init(esp_power_level_t power) {
   NimBLEDevice::init(m_ID);
   NimBLEDevice::setPower(power);
   NimBLEDevice::setSecurityAuth(true, false, true);
+  NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_YESNO);
   // NimBLEDevice::setOwnAddrType(BLE_OWN_ADDR_PUBLIC);
 }
 
