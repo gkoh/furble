e7d4b779ed34b0d5408a57a6b9637f794a688225
Guo-Rong <5484552+gkoh@users.noreply.github.com>
2025-08-31 10:50:08 +0930

    Increase minimum CPU frequency to 80MHz.
    With esp32, CPU minimum frequency to ensure Bluetooth stability is
    80MHZ.
    This effectively erases any power saving benefit from light sleep.
    This will need to be revisited, however, connection stability is a
    priority over battery life.

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ void app_main() {
 
   esp_pm_config_esp32_t pm_config = {
       .max_freq_mhz = 80,
-      .min_freq_mhz = 40,
+      .min_freq_mhz = 80,
       .light_sleep_enable = true,
   };
   ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
