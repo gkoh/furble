93059a681ea9c220c7735da7ded3f16781e7a6ff
Guo-Rong <5484552+gkoh@users.noreply.github.com>
2025-09-05 13:32:08 +0930

    Refactor Fujifilm into FujifilmBasic and FujifilmSecure. (#212)
    * Refactor Fujifilm into FujifilmBasic and FujifilmSecure.
    
    Cut the Fujifilm implementation into two pieces:
    * FujifilmBasic: for the original, unsecure implementation
    * FujifilmSecure: for the newer, secure implementation
    
    Also, disable the `m_Configured` check, this appears deprecated.
    
    * Update service UUID based on scanning data.
    
    * Add numeric display security capability.
    
    Use the default to auto-accept the displayed PIN.
    This may need to be revisited if users want the actual security.
    
    * Double NimBLE stack size.
    
    * Add status and identifier handling.
    
    * Correct status/ack index byte.
    
    * Adjust security settings.
    
    Correct a leak of NimBLEClient() objects, only create when actually
    needed.
    Adjust connection parameter handling.
    
    * Increase main task stack and run UI in host main task.
    
    This may explain some memory corruption issues.
    
    Refactor Fujifilm advertisement handling and record camera serial from
    advertisement data for secure pairing.
    
    * Initial attempt scanning for previously paired FujifilmSecure.
    
    * Make most of the subscriptions optional.
    
    Only the geotag subscription needs to succeed.
    
    * Use correct advertisement service UUID for re-connect scanning.
    
    * Actually save the camera serial to NVS.
    
    * Set BT own address type to resolvable public address.
    
    Testing shows this still works with Nikon, Sony and FujifilmBasic.
    
    * Configure ID key distribution to ensure persistent RPA bonding.
    
    * Add NimBLE-Arduino patch for peer address type search fix.
    
    Patch the resolved address type in the NimBLE RPA search code.
    
    Thanks to @tiredboffin for the expert assistance here.
    
    * Increase minimum CPU frequency to 80MHz.
    
    With esp32, CPU minimum frequency to ensure Bluetooth stability is
    80MHZ.
    This effectively erases any power saving benefit from light sleep.
    This will need to be revisited, however, connection stability is a
    priority over battery life.
    
    * Ensure NimBLEClient object is properly freed.
    
    * Update platformio.
    
    * Avoid unnecessary creation of Camera objects.
    
    * Update NimBLE-Arduino to latest bugfix release.
    
    Update to NimBLE-Arduino 2.3.5.
    
    * Fix disconnect/reconnect handling.
    
    Improve disconnect detection and reconnection handling.
    This should fix Infinite-ReConnect issues.
    
    Rather than manage the NimBLEClient object, let the library manage its
    own destruction.
    
    * Display version rather than 'furble' for test builds.
    
    Add a test version macro for displaying the version in the header.
    This should only be needed during test builds, remember to disable
    before merging back.
    
    * Forcefully delete bond whether needed or not
    
    isBonded() does not understand RPA, however deleteBond() does.
    Don't bother checking if a bond exists, just delete it and allow the
    function to fail.
    
    * Fix reconnect after initial pairing.
    
    Further record the pairing state so reconnect immediately after initial
    pairing works.
    
    * Move test build version logic into workflow.
    
    Leverage the build system and github actions to handle the test version
    stuff.
    
    * Add some connect/disconnect logging.

--- lib/furble/CameraList.cpp
+++ lib/furble/CameraList.cpp
@@ -4,7 +4,8 @@
 #include "CanonEOSM6.h"
 #include "CanonEOSR.h"
 #include "FauxNY.h"
-#include "Fujifilm.h"
+#include "FujifilmBasic.h"
+#include "FujifilmSecure.h"
 #include "MobileDevice.h"
 #include "Nikon.h"
 #include "Sony.h"
@@ -125,10 +126,8 @@ void CameraList::remove(Furble::Camera *camera) {
 
   m_Prefs.end();
 
-  // delete bond if required
-  if (NimBLEDevice::isBonded(camera->getAddress())) {
-    NimBLEDevice::deleteBond(camera->getAddress());
-  }
+  // delete bond whether needed or not
+  NimBLEDevice::deleteBond(camera->getAddress());
 }
 
 /**
@@ -151,8 +150,9 @@ void CameraList::load(void) {
     m_Prefs.getBytes(i.name, dbuffer, dbytes);
 
     switch (i.type) {
-      case Camera::Type::FUJIFILM:
-        m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Fujifilm(dbuffer, dbytes)));
+      case Camera::Type::FUJIFILM_BASIC:
+        m_ConnectList.push_back(
+            std::unique_ptr<Furble::Camera>(new FujifilmBasic(dbuffer, dbytes)));
         break;
       case Camera::Type::CANON_EOS_M6:
         m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new CanonEOSM6(dbuffer, dbytes)));
@@ -171,6 +171,10 @@ void CameraList::load(void) {
         break;
       case Camera::Type::SONY:
         m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Sony(dbuffer, dbytes)));
+        break;
+      case Camera::Type::FUJIFILM_SECURE:
+        m_ConnectList.push_back(
+            std::unique_ptr<Furble::Camera>(new FujifilmSecure(dbuffer, dbytes)));
         break;
     }
   }
@@ -202,8 +206,8 @@ Furble::Camera *CameraList::get(size_t n) {
 }
 
 bool CameraList::match(const NimBLEAdvertisedDevice *pDevice) {
-  if (Fujifilm::matches(pDevice)) {
-    m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Furble::Fujifilm(pDevice)));
+  if (FujifilmBasic::matches(pDevice)) {
+    m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Furble::FujifilmBasic(pDevice)));
     return true;
   } else if (CanonEOSM6::matches(pDevice)) {
     m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Furble::CanonEOSM6(pDevice)));
@@ -216,6 +220,9 @@ bool CameraList::match(const NimBLEAdvertisedDevice *pDevice) {
     return true;
   } else if (Sony::matches(pDevice)) {
     m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Furble::Sony(pDevice)));
+    return true;
+  } else if (FujifilmSecure::matches(pDevice)) {
+    m_ConnectList.push_back(std::unique_ptr<Furble::Camera>(new Furble::FujifilmSecure(pDevice)));
     return true;
   }
 
--- lib/furble/Fujifilm.cpp
+++ lib/furble/Fujifilm.cpp
@@ -14,13 +14,9 @@ constexpr std::array<uint8_t, 2> Fujifilm::SHUTTER_CMD;
 constexpr std::array<uint8_t, 2> Fujifilm::SHUTTER_PRESS;
 constexpr std::array<uint8_t, 2> Fujifilm::SHUTTER_FOCUS;
 
-void Fujifilm::print_token(const std::array<uint8_t, TOKEN_LEN> &token) {
-  ESP_LOGI(LOG_TAG, "Token = %02x%02x%02x%02x", token[0], token[1], token[2], token[3]);
-}
-
 void Fujifilm::notify(BLERemoteCharacteristic *pChr, uint8_t *pData, size_t length, bool isNotify) {
-  ESP_LOGI(LOG_TAG, "Got %s callback: %u bytes from %s", isNotify ? "notification" : "indication",
-           length, pChr->getUUID().toString().c_str());
+  ESP_LOGI(LOG_TAG, "Got %s (%u bytes) from %s", isNotify ? "notification" : "indication", length,
+           pChr->getUUID().toString().c_str());
   if (length > 0) {
     ESP_LOGI(LOG_TAG, " %s", NimBLEUtils::dataToHexString(pData, length).c_str());
   }
@@ -34,11 +30,11 @@ void Fujifilm::notify(BLERemoteCharacteristic *pChr, uint8_t *pData, size_t leng
       m_GeoRequested = true;
     }
   } else {
-    ESP_LOGW(LOG_TAG, "Unhandled notification handle.");
+    ESP_LOGW(LOG_TAG, "Unhandled subscription.");
   }
 }
 
-bool Fujifilm::subscribe(const NimBLEUUID &svc, const NimBLEUUID &chr, bool notifications) {
+bool Fujifilm::subscribe(const NimBLEUUID &svc, const NimBLEUUID &chr, bool notification) {
   auto pSvc = m_Client->getService(svc);
   if (pSvc == nullptr) {
     return false;
@@ -50,145 +46,31 @@ bool Fujifilm::subscribe(const NimBLEUUID &svc, const NimBLEUUID &chr, bool noti
   }
 
   return pChr->subscribe(
-      notifications,
+      notification,
       [this](BLERemoteCharacteristic *pChr, uint8_t *pData, size_t length, bool isNotify) {
         this->notify(pChr, pData, length, isNotify);
       },
       true);
 }
 
-Fujifilm::Fujifilm(const void *data, size_t len) : Camera(Type::FUJIFILM, PairType::SAVED) {
-  if (len != sizeof(fujifilm_t))
-    abort();
-
-  const fujifilm_t *fujifilm = static_cast<const fujifilm_t *>(data);
-  m_Name = std::string(fujifilm->name);
-  m_Address = NimBLEAddress(fujifilm->address, fujifilm->type);
-  memcpy(m_Token.data(), fujifilm->token, TOKEN_LEN);
-}
-
-Fujifilm::Fujifilm(const NimBLEAdvertisedDevice *pDevice) : Camera(Type::FUJIFILM, PairType::NEW) {
-  const char *data = pDevice->getManufacturerData().data();
-  m_Name = pDevice->getName();
-  m_Address = pDevice->getAddress();
-  m_Token = {data[3], data[4], data[5], data[6]};
-  ESP_LOGI(LOG_TAG, "Name = %s", m_Name.c_str());
-  ESP_LOGI(LOG_TAG, "Address = %s", m_Address.toString().c_str());
-  print_token(m_Token);
-}
-
 /**
  * Determine if the advertised BLE device is a Fujifilm.
  */
 bool Fujifilm::matches(const NimBLEAdvertisedDevice *pDevice) {
-  if (pDevice->haveManufacturerData() && pDevice->getManufacturerData().length() == ADV_TOKEN_LEN) {
+  if (pDevice->haveManufacturerData()
+      && pDevice->getManufacturerData().length() >= sizeof(fujifilm_adv_t)) {
     const fujifilm_adv_t adv = pDevice->getManufacturerData<fujifilm_adv_t>();
-    if (adv.company_id == ID && adv.type_token == TYPE_TOKEN) {
-      return true;
-    }
+    return (adv.company_id == COMPANY_ID);
   }
   return false;
-}
-
-/**
- * Connect to a Fujifilm.
- *
- * During initial pairing the advertisement includes a pairing token, this token
- * is what we use to identify ourselves upfront and during subsequent
- * re-pairing.
- */
-bool Fujifilm::_connect(void) {
-  m_Progress = 10;
-
-  NimBLERemoteService *pSvc = nullptr;
-  NimBLERemoteCharacteristic *pChr = nullptr;
-
-  ESP_LOGI(LOG_TAG, "Connecting");
-  if (!m_Client->connect(m_Address))
-    return false;
-
-  ESP_LOGI(LOG_TAG, "Connected");
-  m_Progress = 20;
-  pSvc = m_Client->getService(SVC_PAIR_UUID);
-  if (pSvc == nullptr)
-    return false;
-
-  ESP_LOGI(LOG_TAG, "Pairing");
-  pChr = pSvc->getCharacteristic(CHR_PAIR_UUID);
-  if (pChr == nullptr)
-    return false;
-
-  if (!pChr->canWrite())
-    return false;
-  print_token(m_Token);
-  if (!pChr->writeValue(m_Token.data(), m_Token.size(), true))
-    return false;
-  ESP_LOGI(LOG_TAG, "Paired!");
-  m_Progress = 30;
-
-  ESP_LOGI(LOG_TAG, "Identifying");
-  pChr = pSvc->getCharacteristic(CHR_IDEN_UUID);
-  if (!pChr->canWrite())
-    return false;
-  const auto name = Device::getStringID();
-  if (!pChr->writeValue(name.c_str(), name.length(), true))
-    return false;
-  ESP_LOGI(LOG_TAG, "Identified!");
-  m_Progress = 40;
-
-  // indications
-  ESP_LOGI(LOG_TAG, "Configuring");
-  if (!this->subscribe(SVC_CONF_UUID, CHR_IND1_UUID, false)) {
-    return false;
-  }
-
-  if (!this->subscribe(SVC_CONF_UUID, CHR_IND2_UUID, false)) {
-    return false;
-  }
-  m_Progress = 50;
-
-  // wait for up to (10*500)ms callback
-  for (unsigned int i = 0; i < 10; i++) {
-    if (m_Configured) {
-      break;
-    }
-    m_Progress = m_Progress.load() + 1;
-    vTaskDelay(pdMS_TO_TICKS(500));
-  }
-
-  m_Progress = 60;
-  // notifications
-  if (!this->subscribe(SVC_CONF_UUID, CHR_NOT1_UUID, true)) {
-    return false;
-  }
-  m_Progress = 70;
-
-  if (!this->subscribe(SVC_CONF_UUID, CHR_NOT2_UUID, true)) {
-    return false;
-  }
-  m_Progress = 80;
-
-  if (!this->subscribe(SVC_CONF_UUID, CHR_IND3_UUID, false)) {
-    return false;
-  }
-  m_Progress = 100;
-
-  ESP_LOGI(LOG_TAG, "Configured");
-  ESP_LOGI(LOG_TAG, "Connected");
-
-  return true;
 }
 
 template <std::size_t N>
 void Fujifilm::sendShutterCommand(const std::array<uint8_t, N> &cmd,
                                   const std::array<uint8_t, N> &param) {
-  NimBLERemoteService *pSvc = m_Client->getService(SVC_SHUTTER_UUID);
-  if (pSvc) {
-    NimBLERemoteCharacteristic *pChr = pSvc->getCharacteristic(CHR_SHUTTER_UUID);
-    if ((pChr != nullptr) && pChr->canWrite()) {
-      pChr->writeValue(cmd.data(), sizeof(cmd), true);
-      pChr->writeValue(param.data(), sizeof(cmd), true);
-    }
+  if (m_Shutter != nullptr && m_Shutter->canWrite()) {
+    m_Shutter->writeValue(cmd.data(), sizeof(cmd), true);
+    m_Shutter->writeValue(param.data(), sizeof(cmd), true);
   }
 }
 
@@ -256,24 +138,6 @@ void Fujifilm::updateGeoData(const gps_t &gps, const timesync_t &timesync) {
 
 void Fujifilm::_disconnect(void) {
   m_Client->disconnect();
-  m_Connected = false;
-}
-
-size_t Fujifilm::getSerialisedBytes(void) const {
-  return sizeof(fujifilm_t);
-}
-
-bool Fujifilm::serialise(void *buffer, size_t bytes) const {
-  if (bytes != sizeof(fujifilm_t)) {
-    return false;
-  }
-  fujifilm_t *x = static_cast<fujifilm_t *>(buffer);
-  strncpy(x->name, m_Name.c_str(), MAX_NAME);
-  x->address = (uint64_t)m_Address;
-  x->type = m_Address.getType();
-  memcpy(x->token, m_Token.data(), TOKEN_LEN);
-
-  return true;
 }
 
 }  // namespace Furble
