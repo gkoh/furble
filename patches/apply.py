93de77e8234464fab95762f31465ab95026e4a1a
Guo-Rong <5484552+gkoh@users.noreply.github.com>
2025-09-06 12:10:24 +0930

    Fix finding client during disconnect event. (#220)
    With target RPA active, addresses change before connection and after
    identify resolution.
    This resulted in a failure to find the right client handle when
    searching by peer address.
    
    Fix this by finding the client by connection handle.

--- patches/apply.py
+++ patches/apply.py
@@ -3,11 +3,16 @@ from os.path import join, relpath
 
 Import("env")
 
-def apply_patches(source, target, env):
+def apply_ble_gap_patch(source, target, env):
   env.Execute("{} --verbose -N -p1 -i {} {}".format("patch", "patches/ble_gap.patch", source[0]))
+
+def apply_ble_gap_disconnect_event_patch(source, target, env):
+  env.Execute("{} --verbose -N -p1 -i {} {}".format("patch", "patches/ble_gap_handle_disconnect_event.patch", source[0]))
 
 for lib in env.GetLibBuilders():
   if lib.name == "NimBLE-Arduino":
     build_dir = relpath(env.subst(lib.build_dir), get_project_dir())
     tgt_path = join(build_dir, "nimble", "nimble", "host", "src", "ble_gap.c.o")
-    env.AddPreAction(tgt_path, apply_patches)
+    env.AddPreAction(tgt_path, apply_ble_gap_patch)
+    tgt_path = join(build_dir, "NimBLEClient.cpp.o")
+    env.AddPreAction(tgt_path, apply_ble_gap_disconnect_event_patch)
